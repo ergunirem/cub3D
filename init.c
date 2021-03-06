676952cf86c2c007d1807000cc4dae0a8ef222d4
ergunirem <iremthedeveloper@gmail.com>
2021-03-07 00:19:06 +0300

    all inits in one file

--- init.c
+++ init.c
@@ -6,14 +6,33 @@
 /*   By: icikrikc <icikrikc@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/02/26 20:49:48 by icikrikc      #+#    #+#                 */
-/*   Updated: 2021/03/03 17:38:27 by icikrikc      ########   odam.nl         */
+/*   Updated: 2021/03/06 23:12:09 by icikrikc      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+t_window	*init_window(void)
+{
+	t_window *new_window;
+
+	new_window = (t_window*)malloc(sizeof(t_window));
+	if (!new_window)
+		ft_exit("init malloc failed\n");
+	ft_bzero(new_window, sizeof(t_window));
+	new_window->mlx = mlx_init();
+	if (!new_window->mlx)
+		ft_exit("mlx_init failed\n");
+	new_window->height = 0;
+	new_window->width = 0;
+	new_window->screenshot = 0;
+	return (new_window);
+
+}
+
 void	init_map(t_window *window)
 {
+	t_image *image;
 	if (!(window->map = malloc(sizeof(t_map))))
 		ft_exit("Malloc failed\n");
 	window->map->map_started = FALSE;
@@ -23,6 +42,17 @@ void	init_map(t_window *window)
 	window->map->max_col = 0;
 	window->map->floor_color = -1;
 	window->map->ceiling_color = -1;
+	//put it here for now
+	if (!(image = malloc(sizeof(t_image))))
+		ft_exit("Malloc failed\n");
+	ft_bzero(image, sizeof(t_image));
+	if (!(image->img = mlx_new_image(window->mlx, 640, 480)))
+		ft_exit("mlx_new_image failed\n");
+	image->addr = mlx_get_data_addr(image->img, &image->bits_per_pixel, &image->line_length,
+                                 &image->endian);
+	image->width = 640;
+	image->height = 480;
+	window->image = image;
 }
 
 void	init_textures(t_window *window)
@@ -44,4 +74,22 @@ void	init_textures(t_window *window)
 	window->textures->east->endian = -1;
 	window->textures->west->endian = -1;
 	window->textures->sprite->endian = -1;
+}
+
+int			init_player(t_window *window)
+{
+	if (!(window->player = malloc(sizeof(t_player))))
+		return (-1);
+	ft_bzero(window->player, sizeof(t_player));
+	window->player->pos_x = -20.0;
+	window->player->pos_y = -20.0;
+	window->player->speed = 0.10;
+	window->player->dir_x = 1.0;
+	window->player->dir_y = 0.0;
+	window->player->plane_x = -20.0;
+	window->player->plane_y = -20.0;
+	window->player->rotate_speed = 0.10;
+	window->player->cam_height = 1.0;
+	window->player->health = 20;
+	return (0);
 }
