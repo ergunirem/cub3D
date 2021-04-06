9b4cb362dc8c885727869d4d54362215a04afff5
Irem Cikrikcioglu <icikrikc@f0r6s8.codam.nl>
2021-04-06 17:02:49 +0200

    notes on sprite

---
+++ sprite.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   sprite.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: icikrikc <icikrikc@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/04/06 15:37:02 by icikrikc      #+#    #+#                 */
+/*   Updated: 2021/04/06 16:07:28 by icikrikc      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+//implement bubble sort
+void	sort_sprites()
+{
+
+}
+
+void	handle_sprite()
+{
+
+}
+
+void	draw_sprites()
+{
+
+}
