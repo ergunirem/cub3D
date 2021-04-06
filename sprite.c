511d5d324d05abf2fd8bb35358d5175e153b818f
Irem Cikrikcioglu <icikrikc@f0r6s8.codam.nl>
2021-04-06 17:05:13 +0200

    notes

--- sprite.c
+++ sprite.c
@@ -6,7 +6,7 @@
 /*   By: icikrikc <icikrikc@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/06 15:37:02 by icikrikc      #+#    #+#                 */
-/*   Updated: 2021/04/06 16:07:28 by icikrikc      ########   odam.nl         */
+/*   Updated: 2021/04/06 17:04:53 by icikrikc      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	sort_sprites()
 
 }
 
+//save sprites in a linked list? and save its position in the map?
 void	handle_sprite()
 {
 
@@ -25,5 +26,13 @@ void	handle_sprite()
 
 void	draw_sprites()
 {
-
+	//sort the sprites
+	//draw sprites one by one with a loop that goes through the linked list
+		//calculate sprite_x and sprite_y
+		//further calculations
+		  //intDev, transform_x, transform_y, sprite_screen_x, sprite_height
+		  //draw_start_y, draw_end_y, draw_start_x, draw_end_y
+		  //check out stripe value?
+		//pixels from string and black check
+	//free the linked list
 }
