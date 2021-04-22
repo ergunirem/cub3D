3f3b1f98f53d48da740ee6fba720b1e191055184
ergunirem <iremthedeveloper@gmail.com>
2021-04-23 00:15:05 +0200

    sprite list handling improved & lst_free corrected but not totally debugged

--- handle_sprites.c
+++ handle_sprites.c
@@ -6,24 +6,23 @@
 /*   By: icikrikc <icikrikc@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/09 21:18:14 by icikrikc      #+#    #+#                 */
-/*   Updated: 2021/04/22 16:34:25 by icikrikc      ########   odam.nl         */
+/*   Updated: 2021/04/23 00:14:03 by icikrikc      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	my_lstfree(t_sprite *ptr_lst)
+void	my_lstfree(t_sprite **ptr_lst)
 {
 	t_sprite	*temp;
 
-	if (!ptr_lst)
+	if (!ptr_lst || !*ptr_lst)
 		return ;
-	while (!ptr_lst->next)
+	while (*ptr_lst)
 	{
-		temp = ptr_lst->next;
-		if (ptr_lst->x > 0 && ptr_lst->y > 0)
-			free(ptr_lst);
-		ptr_lst = temp;
+		temp = *ptr_lst;
+		free(*ptr_lst);
+		*ptr_lst = temp;
 	}
 	return ;
 }
@@ -65,14 +64,6 @@ void	my_lstadd_back(t_sprite *lst, t_sprite *new)
 			free(new);
 			return ;
 		}
-		// if (tmp->x == 0 && tmp->y == 0)
-		// {
-		// 	tmp->x = new->x;
-		// 	tmp->y = new->y;
-		// 	tmp->dist = new->dist;
-		// 	free(new);
-		// 	return ;
-		// }
 		tmp = tmp->next;
 	}
 	last = my_lstlast(lst);
@@ -94,13 +85,16 @@ void	handle_sprite(t_window *window, t_ray *ray, t_sprite *s_list)
 		exit_w_message("malloc failed in handle_sprite\n", 1, window);
 	sprite->x = ray->map_x;
 	sprite->y = ray->map_y;
-	sprite->dist = ray->perp_wall_dist;
+	sprite->dist = ((window->player->pos_x - sprite->x) * (window->player->pos_x
+				- sprite->x) + (window->player->pos_y
+				- sprite->y) * (window->player->pos_y - sprite->y));
 	sprite->next = NULL;
-	// head = s_list;
-	// if (head->x == 0 && head->y == 0)
-	// {
-	// 	head->x = ray->map_x;
-	// 	head->y = ray->map_y;
-	// }
-	my_lstadd_back(window->s_info->s_list, sprite);
+	head = window->s_list;
+	if (head->x == -1 && head->y == -1)
+	{
+		head->x = sprite->x;
+		head->y = sprite->x;
+		head->dist = sprite->dist;
+	}
+	my_lstadd_back(window->s_list, sprite);
 }
