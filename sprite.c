7b2e61770fdd7bab8a0542796ccc08a81d426ead
ergunirem <iremthedeveloper@gmail.com>
2021-04-23 02:27:54 +0200

    dist & sort problem solved

--- sprite.c
+++ sprite.c
@@ -6,32 +6,22 @@
 /*   By: icikrikc <icikrikc@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/04/06 15:37:02 by icikrikc      #+#    #+#                 */
-/*   Updated: 2021/04/22 16:13:44 by icikrikc      ########   odam.nl         */
+/*   Updated: 2021/04/23 02:24:10 by icikrikc      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-t_sprite *swap(t_sprite *s_one, t_sprite *s_two)
+t_sprite	*swap(t_sprite *s_one, t_sprite *s_two, int *swapped)
 {
-	t_sprite *tmp;
+	t_sprite	*tmp;
 
 	tmp = s_two->next;
 	s_two->next = s_one;
 	s_one->next = tmp;
+	*swapped = 1;
 	return (s_two);
 }
-
-// void print_list(t_sprite *s_list, int i)
-// {
-// 	while (s_list)
-// 	{
-// 		printf("%d x: %f y: %f\n", i, s_list->x, s_list->y);
-// 		// printf("Dist %d: %f\n", i, s_list->dist);
-// 		s_list = s_list->next;
-// 		i++;
-// 	}
-// }
 
 /*
 It bubble sorts the sprites list based on distance (perp_wall_dist)
@@ -39,25 +29,22 @@ It bubble sorts the sprites list based on distance (perp_wall_dist)
 
 void	sort_sprites(int num_sprites, t_sprite **s_list, int i, int j)
 {
-	t_sprite **head;
-	t_sprite *s_one;
-	t_sprite *s_two;
-	int	swapped;
+	t_sprite	**head;
+	t_sprite	*s_one;
+	t_sprite	*s_two;
+	int			swapped;
 
 	while (i <= num_sprites)
 	{
 		head = s_list;
 		swapped = 0;
 		j = 0;
-		while (j <= num_sprites - i - 1)
+		while (j < num_sprites - i - 1)
 		{
 			s_one = *head;
 			s_two = s_one->next;
-			if (s_one->dist > s_two->dist)
-			{
-				*head = swap(s_one, s_two);
-				swapped = 1;
-			}
+			if (s_one->dist < s_two->dist)
+				*head = swap(s_one, s_two, &swapped);
 			head = &(*head)->next;
 			j++;
 		}
@@ -81,21 +68,21 @@ void	do_projection(t_sprite_info *s, t_window *window, t_sprite *sprite)
 	s->sprite_x = sprite->x - (p->pos_x - 0.5);
 	s->sprite_y = sprite->y - (p->pos_y - 0.5);
 	s->inv_det = 1.0 / (p->plane_x * p->dir_y - p->dir_x * p->plane_y);
-	s->transform_x = s->inv_det * (p->dir_y * s->sprite_x - p->dir_x * s->sprite_y);
-	s->transform_y = s->inv_det * (-p->plane_y * s->sprite_x + p->plane_x * s->sprite_y);
-	s->sprite_screen_x = (int)(window->width / 2) * (1 + s->transform_x / s->transform_y);
-	s->sprite_height = abs((int)(window->height / s->transform_y));
-	s->draw_start_y = -s->sprite_height / 2 + window->height / 2;
+	s->trans_x = s->inv_det * (p->dir_y * s->sprite_x - p->dir_x * s->sprite_y);
+	s->trans_y = s->inv_det * (-p->plane_y * s->sprite_x + p->plane_x * s->sprite_y);
+	s->s_screen_x = (int)(window->width / 2) * (1 + s->trans_x / s->trans_y);
+	s->s_height = abs((int)(window->height / s->trans_y));
+	s->draw_start_y = -s->s_height / 2 + window->height / 2;
 	if (s->draw_start_y < 0)
 		s->draw_start_y = 0;
-	s->draw_end_y = s->sprite_height / 2 + window->height / 2;
+	s->draw_end_y = s->s_height / 2 + window->height / 2;
 	if (s->draw_end_y >= window->height)
 		s->draw_end_y = window->height - 1;
-	s->sprite_width = abs((int)(window->height / s->transform_y));
-	s->draw_start_x = -s->sprite_width / 2 + s->sprite_screen_x;
+	s->s_width = abs((int)(window->height / s->trans_y));
+	s->draw_start_x = -s->s_width / 2 + s->s_screen_x;
 	if (s->draw_start_x < 0)
 		s->draw_start_x = 0;
-	s->draw_end_x = s->sprite_width / 2 + s->sprite_screen_x;
+	s->draw_end_x = s->s_width / 2 + s->s_screen_x;
 	if (s->draw_end_x >= window->width)
 		s->draw_end_x = window->width - 1;
 }
@@ -108,26 +95,27 @@ ZBuffer, with perpendicular distance, then it loops through every pixel of the s
 gets the color for each pixel and paints it to the window if not black.
 */
 
-void	draw_vertical_stripe(t_sprite_info *s, t_ray *ray, t_window *window)
+void	draw_vertical_stripe(t_sprite_info *s, t_ray *ray, t_window *win)
 {
 	int	color;
 
 	s->stripe = s->draw_start_x;
 	while (s->stripe < s->draw_end_x)
 	{
-		s->tex_x = (int)(256 * (s->stripe - (-s->sprite_width / 2 + s->sprite_screen_x))
-			* window->textures->sprite->width / s->sprite_width) / window->textures->sprite->line_length;
-		if (s->transform_y > 0 && s->stripe > 0 && s->stripe < window->width
-			&& s->transform_y < ray->z_buffer[s->stripe])
+		s->tex_x = (int)(256 * (s->stripe - (-s->s_width / 2 + s->s_screen_x))
+				* win->textures->sprite->width / s->s_width)
+			/ win->textures->sprite->line_length;
+		if (s->trans_y > 0 && s->stripe > 0 && s->stripe < win->width
+			&& s->trans_y < ray->z_buffer[s->stripe])
 		{
 			s->y = s->draw_start_y;
 			while (s->y < s->draw_end_y)
 			{
-				s->d = (s->y) * 256 - window->height * 128 + s->sprite_height * 128;
-				s->tex_y = ((s->d * 64) / s->sprite_height) / 256; //change 64 here: it's actually tex_height but there is a problem with it
-				color = my_mlx_pixel_get(window->textures->sprite, s->tex_x, s->tex_y);
+				s->d = (s->y) * 256 - win->height * 128 + s->s_height * 128;
+				s->tex_y = ((s->d * 64) / s->s_height) / 256; //change 64 here: it's actually tex_height but there is a problem with it
+				color = my_mlx_pixel_get(win->textures->sprite, s->tex_x, s->tex_y);
 				if ((color & 0x00FFFFFF) != 0)
-					my_mlx_pixel_set(window->image, s->stripe, s->y, color);
+					my_mlx_pixel_set(win->image, s->stripe, s->y, color);
 				s->y++;
 			}
 		}
@@ -143,20 +131,17 @@ void	draw_sprites(t_window *window, t_ray *ray)
 {
 	t_sprite_info	*s;
 	t_sprite		*s_list;
-	t_player		*p;
+	t_sprite		*tmp_list;
 	int				i;
 
-	// if (!s->s_list)
-	// 	ft_exit("empty sprite list\n", window);
-	s = window->s_info;
-	s_list = s->s_list;
+	s = malloc(sizeof(t_sprite_info));
+	if (!s)
+		ft_exit("Malloc failed\n", window);
+	ft_bzero(s, sizeof(t_sprite_info));
+	s_list = window->s_list;
+	tmp_list = s_list;
 	s->num_sprites = my_lstsize(s_list);
-	printf("num:%d\n", s->num_sprites);
-	// printf("before\n");
-	// print_list(s_list, 0);
-	// sort_sprites(s->num_sprites, &s_list, 0, 0);
-	// printf("after\n");
-	// print_list(s_list, 0);
+	sort_sprites(s->num_sprites, &s_list, 0, 0);
 	i = 0;
 	while (i < s->num_sprites)
 	{
@@ -165,6 +150,6 @@ void	draw_sprites(t_window *window, t_ray *ray)
 		s_list = s_list->next;
 		i++;
 	}
-	// if (s_list->next)
-	// 	my_lstfree(s_list); //segv when no sprite is available
+	free(s);
+	my_lstfree(&tmp_list);
 }
